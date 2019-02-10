7dd2259f0e78b0dc6410ccb8e1591382efaac552
Justin Paul <justin@jpaul.me>
2019-02-09 20:57:21 -0500

    Create rPiVpgUnicornHat.py
    Initial Commit of Python Script to read VPG status from Zerto Analytics and write them to a Pimoroni Unicorn Hat

---
+++ rPiVpgUnicornHat.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import time
+import unicornhat as unicorn
+
+zaUser = "email@addr.com"
+zaPass = "password"
+
+api_url_base = "https://analytics.api.zerto.com"
+api_token = ''
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(180)
+unicorn.brightness(0.2)
+width,height=unicorn.get_shape()
+
+unicorn.set_pixel(0,0,255,255,255)
+unicorn.show()
+
+def za_authorize(Username, Password):
+    api_url = '{0}/v2/auth/token'.format(api_url_base)
+    headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
+    creds = dict(
+        username=Username,
+        password=Password
+    )
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(creds))
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))["token"]
+    else:
+        return None
+
+def za_get_vpgs_health(api_token):
+    api_url = '{0}/v2/monitoring/vpgs'.format(api_url_base)
+    headers = {'accept': 'application/json', 'Authorization': 'Bearer ' + api_token}
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+def draw_vpgs(healthy, warn, error):
+    total = healthy + warn + error
+
+    for y in range(height):
+        for x in range(width):
+            if error > 0:
+               unicorn.set_pixel(x,y,255,0,0)
+               error -= 1
+            elif warn > 0:
+                unicorn.set_pixel(x,y,255,255,0)
+                warn -= 1
+            elif healthy > 0:
+                unicorn.set_pixel(x,y,0,255,0)
+                healthy -= 1
+            else:
+                unicorn.set_pixel(x,y,0,0,0)
+
+    unicorn.show()
+
+# main loop
+
+while True:
+    api_token = za_authorize(zaUser, zaPass)
+    vpgs = za_get_vpgs_health(api_token)
+
+    healthy = vpgs["healthyVpgsCount"]
+    warning = vpgs["warnedVpgsCount"]
+    error = vpgs["erroneousVpgsCount"]
+
+    draw_vpgs(healthy, warning, error)
+
+    time.sleep(30)
